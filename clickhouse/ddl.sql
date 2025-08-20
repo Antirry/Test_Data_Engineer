cccb5aff4fdac1f98699873805bcc1f27107c1d1
Antipin Dmitry <103675440+Antirry@users.noreply.github.com>
2025-08-20 10:25:01 +0300

    Update ddl.sql

--- clickhouse/ddl.sql
+++ clickhouse/ddl.sql
@@ -1,39 +1,37 @@
--- Таблица для хранения показов рекламы (impressions)
-CREATE TABLE raw_impressions (
-    req_id UUID,                  -- ( Лучше UUID, чем строка)
-    user_id UInt64,                -- (целое, без знака)
-    campaign_id UInt32,
-    creative_id UInt32,
-    ip IPv4,                       -- (IPv4 экономит место и ускоряет фильтрацию)
-    ua String,
-    ts DateTime
-) ENGINE = MergeTree()
-PARTITION BY toYYYYMMDD(ts)        -- Партиционирование по дате события для быстрой очистки и выборок
-ORDER BY (ts, req_id)              -- Сортировка по времени, затем по уникальному ID
-SETTINGS index_granularity = 8192; -- Размер гранулы индекса (по умолчанию), можно настроить под объёмы
+CREATE DATABASE IF NOT EXISTS default;
 
--- Таблица для хранения кликов по рекламе
-CREATE TABLE raw_clicks (
-    req_id UUID,                  
-    user_id UInt64,               
-    campaign_id UInt32,           
-    creative_id UInt32,           
-    ip IPv4,                      
-    ua String,                    
-    ts DateTime                   
+-- События показов: оптимизируем под агрегации по campaign_id и времени
+CREATE TABLE IF NOT EXISTS raw_impressions (
+  req_id String,
+  user_id String,
+  campaign_id String,
+  creative_id String,
+  ip String,
+  ua String,
+  ts DateTime
 ) ENGINE = MergeTree()
-PARTITION BY toYYYYMMDD(ts)     -- Партиционирование по дате события для быстрой очистки и выборок   
-ORDER BY (ts, req_id)              
-SETTINGS index_granularity = 8192 -- Размер гранулы индекса (по умолчанию), можно настроить под объёмы;
+PARTITION BY toYYYYMM(ts)
+ORDER BY (campaign_id, ts, req_id);
 
--- Таблица для хранения сработок системы антифрода
-CREATE TABLE fraud_alerts (
-    ts DateTime,
-    ip IPv4,
-    clicks UInt32,
-    window_start DateTime,
-    window_end DateTime
+-- События кликов: оптимизируем под проверки по ip и времени
+CREATE TABLE IF NOT EXISTS raw_clicks (
+  req_id String,
+  user_id String,
+  campaign_id String,
+  creative_id String,
+  ip String,
+  ua String,
+  ts DateTime
 ) ENGINE = MergeTree()
-PARTITION BY toYYYYMMDD(window_start) -- Партиция по дню старта окна (можно выбрать другой критерий)
-ORDER BY (window_start, ip) 
-SETTINGS index_granularity = 8192 -- Размер гранулы индекса (по умолчанию), можно настроить под объёмы;
+PARTITION BY toYYYYMM(ts)
+ORDER BY (ip, ts, req_id);
+
+CREATE TABLE IF NOT EXISTS fraud_alerts (
+  ts DateTime,             -- момент фиксации правила
+  ip String,
+  clicks UInt32,
+  window_start DateTime,
+  window_end DateTime
+) ENGINE = MergeTree()
+PARTITION BY toYYYYMM(ts)
+ORDER BY (ip, window_start, window_end);
